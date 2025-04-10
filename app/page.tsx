4baef1759a7f52da9e83dc92111c0c2e3e93c8e6
Anthony <91166746+anthonyjV1@users.noreply.github.com>
2025-04-09 20:53:11 -0400

    .

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import Link from 'next/link'
+import React from 'react'
+
+const HomePage = () => {
+  return (
+    <button>
+        <Link href="/sign-in">Sign IN</Link>
+    </button>
+  )
+}
+
+export default HomePage
