3f4621211d1176a9a9d02e5283a90e3d109ad69a
Philip William Ventura <philipwilliamventura@gmail.com>
2025-04-14 18:57:20 -0400

    form change

--- app/(auth)/sign-up/page.tsx
+++ app/(auth)/sign-up/page.tsx
@@ -11,7 +11,7 @@ const page = () => {
       
       {/* Content overlay */}
       <div className="absolute inset-0 flex items-center justify-center z-10">
-        <div className="text-center max-w-md px-6 py-8 bg-gray-900/70 backdrop-blur-sm rounded-2xl border border-indigo-500/20 shadow-xl">
+        <div>
           </div>
           
           
