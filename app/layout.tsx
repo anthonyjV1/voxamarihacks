4cdda118f0ff0a0c4de154f70123a2b05a0b9699
Anthony <91166746+anthonyjV1@users.noreply.github.com>
2025-04-15 08:33:45 -0400

    favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,25 +8,24 @@ const monaSans = Mona_Sans({
   subsets: ["latin"],
 });
 
-
-
 export const metadata: Metadata = {
   title: "Voxa",
   description: "Ai powered platform for preparing for mock interviews.",
+  icons: {
+    icon: "/favicon.ico",  // Reference to your favicon file in the public folder
+  },
 };
 
 export default function RootLayout({
   children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+}: Readonly<{ children: React.ReactNode }>) {
   return (
-    <html lang="en" className ="dark">
-      <body
-        className={`${monaSans.className} antialiased pattern`}>
-      {children}
-      <Toaster/>
+    <html lang="en" className="dark">
+      <body className={`${monaSans.className} antialiased pattern`}>
+        {children}
+        <Toaster />
       </body>
     </html>
   );
 }
+
